5152071781c9b4dc5af5a701e12627487a13ef21
Laszlo Simon <laszlo.simon@t-systems.com>
2016-09-24 22:56:50 +0200

    midleware skeleton

---
+++ winston-express.js
@@ -0,0 +1,12 @@
+"use strict";
+const fs = require('fs');
+const path = require('path');
+const winston = require('winston');
+
+module.exports = exports = (function(fs, path, winston){
+    const simplifiers = require('./assets/')
+
+    return function(req, res, next){
+
+    }
+})(fs, path, winston);
